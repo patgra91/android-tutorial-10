418a0ad6b3b01bc4abfab73dc410f1ca1a3605f5
Patrick Granberg <patgra91@gmail.com>
2020-10-10 19:32:13 +0200

    The shopping cart in the app bar redirects to OrderActivity

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -53,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_order:
                 displayToast(getString(R.string.action_order_message));
+                Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
                 return true;
             case R.id.action_status:
                 displayToast(getString(R.string.action_status_message));
